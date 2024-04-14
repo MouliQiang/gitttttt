22e06b24b22356def2e5b756f3164900b49b88b1
MouLiQiang <19891729276@163.com>
2024-04-14 11:26:17 +0800

    Init

---
+++ src/main/java/com/example/gitttttt/GittttttApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitttttt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GittttttApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GittttttApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gitttttt/GittttttApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitttttt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GittttttApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
